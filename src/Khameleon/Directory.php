2df0a99539e79b223cdae2a6222a980ddfc12972
Niktux <nico.ln@gmail.com>
2013-03-21 00:14:53 +0100

    Update interfaces

--- src/Khameleon/Directory.php
+++ src/Khameleon/Directory.php
@@ -4,5 +4,15 @@ namespace Khameleon;
 
 interface Directory extends Node
 {
+    /**
+     * @returns \Iterator
+     */
+    public function read();
     
+    /**
+     * @param string $name
+     * @returns \Khameleon\Node
+     * @throws \Khameleon\Exceptions\Exception
+     */
+    public function get($name);
 }
--- src/Khameleon/File.php
+++ src/Khameleon/File.php
@@ -4,4 +4,13 @@ namespace Khameleon;
 
 interface File extends Node
 {
+    /**
+     * @returns string
+     */
+    public function read();
+    
+    /**
+     * @param string $content
+     */
+    public function write($content);
 }
