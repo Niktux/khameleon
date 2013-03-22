7e5a0b7030e1485da181164bdfe7660895dd3a3f
Niktux <nico.ln@gmail.com>
2013-03-22 21:36:32 +0100

    Add PHPDoc

--- src/Khameleon/FileSystem.php
+++ src/Khameleon/FileSystem.php
@@ -4,13 +4,48 @@ namespace Khameleon;
 
 interface FileSystem
 {
+    /**
+     * @param string $path
+     * @return \Khameleon\Node
+     * @throws \Khameleon\Exceptions\Exception
+     */
     public function get($path);
+    
+    /**
+     * @param string $path
+     * @return \Khameleon\File
+     */
     public function putFile($path);
+    
+    /**
+     * @param string $path
+     * @return \Khameleon\Directory
+     */
     public function putDirectory($path);
     
+    /**
+     * @param string $path
+     * @return boolean
+     */
     public function exists($path);
     
-    public function createFile($path, $content);
+    /**
+     * Fluid interface
+     *
+     * @param string $path
+     * @param string $content
+     * @return \Khameleon\FileSystem
+     * @throws \Khameleon\Exceptions\Exception
+     */
+    public function createFile($path, $content = null);
+    
+    /**
+     * Fluid interface
+     *
+     * @param string $path
+     * @return \Khameleon\FileSystem
+     * @throws \Khameleon\Exceptions\Exception
+     */
     public function createDirectory($path);
     
     //public function mount($path, Directory $subroot);
